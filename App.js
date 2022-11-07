1362635d7d823e4adc2e40c1d736c9b0661f1362
Daniel Busis <busis.daniel@gmail.com>
2022-11-07 00:02:15 -0500

    Added basic checklist

--- App.js
+++ App.js
@@ -1,22 +1,82 @@
+import Checkbox from 'expo-checkbox';
 import { useState } from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, FlatList } from 'react-native';
+
+const exampleListData = {
+  title: "Find the rare crops",
+  entries: [
+    {
+      id: "id001",
+      content: "Find the red acorn",
+      completed: true,
+    },
+    {
+      id: "id002",
+      content: "Find the orange lemon and this is a really long list item",
+      completed: false,
+    },
+    {
+      id: "id003",
+      content: "Find the blue carrot and this is an even longer list item. In fact, it's comprised of multiple sentences. Here are some more words: test test test test test.",
+      completed: true,
+    },
+    {
+      id: "id004",
+      content: "Find the green tomato",
+      completed: false,
+    },
+  ]
+};
+
+const TodoItem = ({ item, index, setListData }) => {
+  return <View style={[styles.todoListRow, ...(item.completed ? [{backgroundColor: "#ccffcc"}] : []) ]}>
+    <Text style={styles.todoListText}>
+      {item.content}
+    </Text>
+    <Checkbox
+      style={{ width: 24, height: 24 }}
+      value={item.completed}
+      onValueChange={newValue => {
+        setListData(oldData => {
+          oldData.entries[index].completed = newValue;
+          return { ...oldData };
+        });
+      }} />
+  </View>
+}
 
 export default function App() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="dark" backgroundColor={`rgb(${red}, ${green}, ${blue})`} animated={true} />
-      <Text>Choose your own colors!</Text>
-      <View style={styles.horizontalContainer}>
-        <NumberInput setVal={setRed} />
-        <NumberInput setVal={setGreen} />
-        <NumberInput setVal={setBlue} />
+  const [listData, setListData] = useState(exampleListData);
+
+  const page = 1;
+
+  if (page === 0) {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="dark" backgroundColor={`rgb(${red}, ${green}, ${blue})`} animated={true} />
+        <Text>Choose your own colors!</Text>
+        <View style={styles.horizontalContainer}>
+          <NumberInput setVal={setRed} />
+          <NumberInput setVal={setGreen} />
+          <NumberInput setVal={setBlue} />
+        </View>
       </View>
-    </View>
-  );
+    );
+  } else if (page === 1) {
+    return (
+      <View style={[styles.container, { width: "100%" }]}>
+        <StatusBar style="dark" backgroundColor={`rgb(${red}, ${green}, ${blue})`} animated={true} />
+        <FlatList
+          data={listData.entries}
+          renderItem={({ item, index }) => <TodoItem item={item} index={index} setListData={setListData} />}
+        />
+      </View>
+    )
+  }
 }
 
 const NumberInput = ({ setVal = () => { } }) => {
@@ -47,11 +107,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   horizontalContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'flex-start',
     justifyContent: 'center',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    width: '100%',
+  },
+  todoListRow: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    minWidth: "100%",
+    paddingHorizontal: 10
+  },
+  todoListText: {
+    fontSize: 24,
+    flex: 1
   },
   numberInput: {
     borderColor: 'black',
